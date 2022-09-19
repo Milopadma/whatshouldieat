88d872ba41dd812f6836327865d15ad99f1d58d7
mylo <mylo@Milo.localdomain>
2022-09-19 10:25:40 +0800

    added input checks

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,6 @@ export const getStaticProps = async () => {
 
 type Props = {
   allFood: {
-    id: number;
     name: string;
   };
 };
@@ -50,7 +49,7 @@ const Home: React.FC<Props> = (props) => {
   const addNewFood = async (e: React.SyntheticEvent, newFood: string) => {
     e.preventDefault();
     try {
-      const body = newFood ;
+      const body = newFood;
       await fetch("/api/post", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -63,14 +62,30 @@ const Home: React.FC<Props> = (props) => {
 
   const onSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    console.log("submit" + newFood);
-    addNewFood(e, newFood);
+    const newFoodLowercase = newFood.toLowerCase();
+    const allFoodObject: any = props.allFood;
+    //check if the food is already in the database and not an empty string
+    if (
+      allFoodObject.some((item: any) => item.name === newFoodLowercase) ||
+      newFoodLowercase === "" ||
+      //is not a number
+      !isNaN(Number(newFoodLowercase))
+    ) {
+      alert(
+        "This food is already in the database or you didn't enter anything or you entered a number."
+      );
+    } else {
+      //add the new food to the database
+      addNewFood(e, newFoodLowercase);
+      //clear the text input field
+      setNewFood("");
+    }
   };
 
   return (
     <div className="m-8">
       {/* title */}
-      <h1 className="mt-32 text-6xl italic flex justify-center bg-slate-400">
+      <h1 className="mt-32 text-6xl italic flex justify-center">
         what should i eat?
       </h1>
       <h5 className="text-yellow-700 font-serif text-2xl italic flex justify-center">
@@ -81,7 +96,7 @@ const Home: React.FC<Props> = (props) => {
       {/* button */}
       <div className="mt-8 flex justify-center">
         <button
-          className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-6 rounded"
+          className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-8 rounded"
           onClick={async () => {
             const randomFood = showRandomFood(props.allFood);
             setFood(randomFood.name);
