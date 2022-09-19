6c13537b8a887a96ce1a7656a5e3512230b976bf
mylo <mylo@Milo.localdomain>
2022-09-19 10:07:23 +0800

    added add more food to the db functionality

---
+++ pages/api/post/index.tsx
@@ -0,0 +1,11 @@
+import prisma from "../../../lib/prisma";
+
+export default async function handle(req: any, res: any) {
+  const food = req.body;
+  const result = await prisma.food.create({
+    data: {
+      name: food,
+    },
+  });
+  res.json(result);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useState } from "react";
 import Image from "next/image";
 
+import post from "./api/post";
+
 //prisma
 import prisma from "../lib/prisma";
 
 import Footer from "../src/components/Footer";
 
 export const getStaticProps = async () => {
+  //retrieve all entries from the database
   const allFood = await prisma.food.findMany({
     where: {
       id: {
@@ -42,6 +45,27 @@ const imageloader = () => {
 
 const Home: React.FC<Props> = (props) => {
   const [food, setFood] = useState("sushi");
+  const [newFood, setNewFood] = useState("");
+
+  const addNewFood = async (e: React.SyntheticEvent, newFood: string) => {
+    e.preventDefault();
+    try {
+      const body = newFood ;
+      await fetch("/api/post", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log("submit" + newFood);
+    addNewFood(e, newFood);
+  };
 
   return (
     <div className="m-8">
@@ -53,9 +77,7 @@ const Home: React.FC<Props> = (props) => {
         (mau makan apa besok?)
       </h5>
       {/* subtitle */}
-      <h1 className="mt-8 text-4xl flex justify-center">
-        {food}
-      </h1>
+      <h1 className="mt-8 text-4xl flex justify-center">{food}</h1>
       {/* button */}
       <div className="mt-8 flex justify-center">
         <button
@@ -85,28 +107,23 @@ const Home: React.FC<Props> = (props) => {
         want to add more food to the db?
       </h2>
       <div className="flex justify-center 0">
-        <input
-          className="mt-2 italic border-2 border-blue-50 rounded-md p-2"
-          placeholder="food here"
-          type="text"
-        />
+        <form onSubmit={onSubmit}>
+          <input
+            className="mt-2 italic border-2 border-blue-50 rounded-md p-2"
+            placeholder="food here"
+            type="text"
+            value={newFood}
+            onChange={(e) => setNewFood(e.target.value)}
+          />
+          <button
+            className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded"
+            onClick={onSubmit}
+          >
+            add food
+          </button>
+        </form>
       </div>
-      <div className="mt-2 flex justify-center">
-        <button
-          className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded"
-          onClick={async () => {
-            // You can use any data fetching library
-            const res = await fetch("/api/addFood", {
-              method: "POST",
-              body: JSON.stringify({ food }),
-            });
-            const json = await res.json();
-            console.log(json);
-          }}
-        >
-          add food
-        </button>
-      </div>
+      <div className="mt-2 flex justify-center"></div>
       <Footer></Footer>
     </div>
   );
