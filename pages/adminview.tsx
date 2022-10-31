83e5f28454dd06d96b39128df35ed2635326769d
mylo <48256865+Milopadma@users.noreply.github.com>
2022-10-31 22:25:40 +0800

    looks good

--- pages/adminview.tsx
+++ pages/adminview.tsx
@@ -54,7 +54,7 @@ const AdminView: React.FC<AdminViewProps> = ({
     React.useState<Food[]>(bufferFoodArray);
   const { data: session } = useSession();
   const isOperator = operators.some((operator) => {
-    return operator.name === session?.user?.email;
+    return operator.email === session?.user?.email;
   });
   //on component load, store all food in state
   React.useEffect(() => {
@@ -64,7 +64,7 @@ const AdminView: React.FC<AdminViewProps> = ({
     //if not, redirect to home page
     if (session && session.user) {
       const username = session.user.name;
-      const operator = operators.find((operator) => operator.name === username);
+      const operator = operators.find((operator) => operator.email === username);
       if (!operator) {
         window.location.href = "/";
       }
